412080e59df241e3cbe76d7b5faa3e8b4af9b53e
Susheel Kumar <developer.susheel@gmail.com>
2022-09-12 00:35:30 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">WebShop</a>
+      </header>
+      <main>
+        list product
+      </main>
+    </div>
+  );
+}
+
+export default App;
